0f9808ea88509ccc6733a090eb741b979924d43a
Lawrence Ainooson <LakiMedia@Laki-iMac.local>
2026-04-29 18:05:16 +0000

    fix: remove duplicate imports in vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,9 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
   server: {
@@ -18,4 +15,3 @@ export default defineConfig({
     }
   }
 })
-
